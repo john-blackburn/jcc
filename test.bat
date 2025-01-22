b77266201974f6de18971a0baaa5cd1f41383543
John Blackburn <john.blackburn75@gmail.com>
2025-01-22 18:40:20 +0000

    move test c files to own folder

--- test.bat
+++ test.bat
@@ -3,13 +3,13 @@
 del %1.exe
 del %1.s
 
-jcc %1.c
+jcc examples\%1.c
 winmergeu correct_asm\%1.s %1.s
 echo running %1.exe...
 %1.exe
 echo return %errorlevel%
 
-gcc -o %1_gcc.exe %1.c
+gcc -o examples\%1_gcc.exe %1.c
 echo running %1_gcc.exe...
 %1_gcc.exe
 echo gcc return %errorlevel%
